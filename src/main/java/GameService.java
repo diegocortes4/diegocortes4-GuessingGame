bb1159de4b98d9651645dc71f90b800486cfdb33
Diego Cortes <diegocortes4@gmail.com>
2023-06-18 21:26:50 +0000

    Secretnumber

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -24,7 +24,7 @@ public class GameService {
      * TODO: this method should return true if guessNumber is equal to the secretNumber, and false otherwise.
      *
      * @param guessNumber the number that the user has input into their console.
-     * @return true if guessNumber is equal to the secretNumber, false otherwise.
+     * @return true if guessNumber is equal to the secretNumber, false otherwise
      */
     public boolean checkGuess(double guessNumber) {
         if (guessNumber == secretNumber){
